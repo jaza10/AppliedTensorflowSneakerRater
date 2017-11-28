d3dd45f9e392ea99c8ed337038d34cbd4448e0c7
Jan Zawadzki <jaza10@me.com>
2017-11-28 22:32:01 +0100

    Add files via upload

---
+++ tf_utils Kopie.py
@@ -0,0 +1,29 @@
+import h5py
+import numpy as np
+import tensorflow as tf
+import math
+
+def load_dataset():
+    # Insert code from coursera here
+
+
+def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
+        # Insert code from coursera here
+    
+    return mini_batches
+
+def convert_to_one_hot(Y, C):
+       # Insert code from coursera here
+    return Y
+
+
+def predict(X, parameters):
+    
+       # Insert code from coursera here
+    return prediction
+
+def forward_propagation_for_predict(X, parameters):
+       # Insert code from coursera here
+    
+    return Z3
+    
